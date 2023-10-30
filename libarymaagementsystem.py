f44a5f088cd06bc52969bd425689c29bd560fb51
aaditya subedi <aadityasubedi0@gmail.com>
2023-10-30 18:17:42 +0545

    modified version

--- libarymaagementsystem.py
+++ libarymaagementsystem.py
@@ -49,8 +49,47 @@ class Books:
                  ------------------------------------------------
 """)
 
+    def deletebook(self):
+        self.Bookname=input("enter a book name to delete::>")
+        bookstokeep=[]
+        with open("Books.csv" , "r") as file:
+              reader=csv.reader(file)
+              for rows in reader:
+                   if self.Bookname==rows[0]:
+                        continue
+                   else:
+                        bookstokeep.append(rows)
+        with open("Books.csv", "w") as file:
+             writer=csv.writer(file)
+             writer.writerows(bookstokeep)
+             print("!!Book deleted sucessfully!!")
 
 
+    def editbook(self):
+         self.Bookname=input("enter a book to edit::>")
+         editbook=[]
+         booktoremainsame=[]
+         with open("Books.csv","r") as file:
+              reader=csv.reader(file)
+              for rows in reader:
+                   if len(rows) >= 4 and self.Bookname == rows[0]:
+                        self.Bookname=input("enter a new book name::>")
+                        self.gener=input("enter book gener::>")
+                        self.Booknumber=input("enter booknumber::>")
+                        self.price=input("enter new price::>")
+                        editbook=[(self.Bookname,self.gener,self.Booknumber,self.price)]
+
+                   else:
+                        booktoremainsame.append(rows)
+        
+         with open("books.csv","w") as file:
+              writer=csv.writer(file)
+              writer.writerows(booktoremainsame)
+              writer.writerows(editbook)
+              print("!!Book edited sucessfully!!")
+
+                   
+                      
 def call():
     b1=Books( )
     
@@ -59,7 +98,8 @@ def call():
                 1. Add Books 
                 2. Search Books
                 3. show all books
-                4. Exit
+                4. edit
+                5. delete
 """)
     choice = int (input("Enter your Choice ::>"))
     if choice==1:
@@ -70,6 +110,12 @@ def call():
     
     elif choice==3:
          b1.showbooks()
+    elif choice==4:
+         b1.editbook()
+
+    elif choice==5:
+         b1.deletebook()
+    
     
     else:
         exit(0)
